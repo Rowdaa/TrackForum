c40094b189b00b7deb345c7319a1cde910723b23
rowda <rowdaabdella1@gmail.com>
2023-12-21 02:37:07 +0300

    add auth middleware and protect routes

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,6 @@
 const dbconnection = require("../db/dbconfig");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 async function register(req, res) {
   const { username, firstname, lastname, email, password } = req.body;
@@ -30,9 +32,12 @@ async function register(req, res) {
       if (user.length > 0) {
         return res.status(400).json("msg:you're already registered");
       }
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+
       await dbconnection.query(
         "INSERT INTO  users(username ,firstname, lastname, email, password) VALUES(?,?,?,?,?)",
-        [username, firstname, lastname, email, password]
+        [username, firstname, lastname, email, hashedPassword]
       );
       return res.status(201).json("msg:user created successfully");
     } catch (error) {
@@ -43,9 +48,43 @@ async function register(req, res) {
     }
   }
 }
-function login(req, res) {
-  res.send("login");
+
+async function login(req, res) {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json("msg:please provide all necessary information");
+  } else {
+    try {
+      const [user] = await dbconnection.query(
+        "SELECT username, userId, password from users WHERE email = ?",
+        [email, password]
+      );
+      if (user.length == 0) {
+        return res.status(400).json("msg:This user is not registered");
+      } else {
+        const isMatch = await bcrypt.compare(password, user[0].password);
+        if (!isMatch) {
+          return res
+            .status(400)
+            .json("msg:please provide all necessary information");
+        } else {
+          const username = user[0].username;
+          const userid = user[0].userId;
+          const token = jwt.sign({ username, userid }, "secret", {
+            expiresIn: "1day",
+          });
+          return res
+            .status(200)
+            .json({ msg: "You're logged in successfully", token });
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+      return res.status(500).json("msg:something went wrong");
+    }
+  }
 }
+
 function checkUser(req, res) {
   res.send("check");
 }
